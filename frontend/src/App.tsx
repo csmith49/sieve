f2a8bad81c586322a53b8a964830c962141705ed
Calvin Smith <email@cjsmith.io>
2024-10-24 10:20:08 -0600

    biiiiig changes to frontend components -- shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,34 @@
 import { useEffect, useState } from "react";
+
+import { Button } from "./components/ui/button";
 import {
-  Typography,
-  Button,
   Card,
-  List,
-  ListItem,
-  CardActions,
-} from "@mui/joy";
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+} from "./components/ui/card";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "./components/ui/collapsible";
+import { Separator } from "./components/ui/separator";
+import { AppSidebar } from "./components/app-sidebar";
+import {
+  SidebarProvider,
+  SidebarInset,
+  SidebarTrigger,
+} from "./components/ui/sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+  BreadcrumbPage,
+} from "./components/ui/breadcrumb";
+
 import "./App.css";
 
 type PaperData = {
@@ -46,11 +68,21 @@ function Paper(props: { id: string }) {
 
   return (
     <Card>
-      <Typography level="h3">{data?.title}</Typography>
-      <Typography>{data?.abstract}</Typography>
-      <CardActions>
+      <CardHeader>
+        <CardTitle>{data?.title}</CardTitle>
+      </CardHeader>
+      <Separator />
+      <CardContent>
+        <Collapsible>
+          <CollapsibleTrigger>
+            <p>abstract</p>
+          </CollapsibleTrigger>
+          <CollapsibleContent>{data?.abstract}</CollapsibleContent>
+        </Collapsible>
+      </CardContent>
+      <CardFooter>
         <Button onClick={handleLiked}>{isLiked ? "YES" : "NO"}</Button>
-      </CardActions>
+      </CardFooter>
     </Card>
   );
 }
@@ -69,16 +101,33 @@ function App() {
   const activePapers = papers.slice(0, 10);
 
   return (
-    <div>
-      <List>
-        {activePapers.map((paperId) => (
-          <ListItem key={paperId}>
-            <Paper id={paperId} />
-          </ListItem>
-        ))}
-      </List>
-    </div>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="#">
+                  Building Your Application
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4">
+          {activePapers.map((paperId) => (
+            <Paper key={paperId} id={paperId} />
+          ))}
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
-
 export default App;
